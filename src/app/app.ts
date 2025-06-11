e9b989be017e338e18511d81a36bd41093d90329
Katharina Baron <katharina.baron-forero@actaport.de>
2025-06-11 15:23:05 +0200

    remove unused import

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {TodoComponent} from './todo/todo.component';
-import {AddTodoFormComponent} from './todo/add-todo-form/add-todo-form.component';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +8,7 @@ import {AddTodoFormComponent} from './todo/add-todo-form/add-todo-form.component
   imports: [
     RouterOutlet,
     TodoComponent,
-    AddTodoFormComponent,
+
   ],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
